8aceb809d1b65c595ae5c407c8df29c5a3d8ade3
diego <dgcuenca@gmail.com>
2022-12-14 15:51:46 -0500

    Set up remove test

--- src/Jsmodules/Features.test.js
+++ src/Jsmodules/Features.test.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
-import {add} from './Features.js';
+import {add, remove} from './Features.js';
 
 describe('add', () => {
   test('Correctly add a task', () => {
@@ -25,5 +25,29 @@ describe('add', () => {
     add();
     const list = document.querySelectorAll('.list');
     expect(list).toHaveLength(1);
+  });
+});
+
+describe('remove', () => {
+  test('Correctly remove a task', () => {
+    document.body.innerHTML = `
+    <div class="listContainer">
+      <section class="headerList lineStyle">
+        <h2 class="listTitle" contenteditable="true">Today's To Do</h2>
+        <span class="arrow-ccw reload"></span>
+      </section>
+      <form class="input lineStyle">
+        <input type="text" placeholder="+ Add to your list..." value="test">
+        <button type="submit" class="enter"></button>
+      </form>
+      <section class="toDoListContainer"></section>
+      <div class="clear lineStyle">
+        <button class="clearBtn" type="button">Clear all completed</button>
+      </div>
+    </div>
+    `;
+    remove(0);
+    const list = document.querySelectorAll('.list');
+    expect(list).toHaveLength(0);
   });
 });
